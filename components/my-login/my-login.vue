8711e2653f866acda52e0606fb03bff91213d046
dehaohuang <gredbird@gmai.com>
2023-11-02 21:02:55 -0400

    banner pics

--- components/my-login/my-login.vue
+++ components/my-login/my-login.vue
@@ -1,96 +1,110 @@
 <template>
-    <view class="login-container">
-        <uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
+	<view class="login-container">
+		<uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
 
-        <button type="primary" class="btn-login" open-type="getUserInfo" @getuserinfo="getUserInfo">登录</button>
-        <text class="tips-text">申请获取以下权限</text>
-        <text class="tips-text">获得你的公开信息(昵称、头像、地区等)</text>
-  </view>
+		<button type="primary" class="btn-login" open-type="getUserInfo" @getuserinfo="getUserInfo">登录</button>
+		<text class="tips-text">申请获取以下权限</text>
+		<text class="tips-text">获得你的公开信息(昵称、头像、地区等)</text>
+	</view>
 </template>
 
 <script>
-    import {mapMutations, mapState} from 'vuex'
-    export default {
-        name: "my-login",
-        data() {
-            return {
+	import {
+		mapMutations,
+		mapState
+	} from 'vuex'
+	export default {
+		name: "my-login",
+		data() {
+			return {
 				orderList: [],
 				ordersNonValide: [],
 				ordersNonPayer: [],
 				ordersNonLivrer: [],
 				ordersRembourse: [],
 				warehouseRequestList: []
-            };
-        },
-		computed: {
-		    ...mapState('m_user', ['token', 'openid'])
+			};
 		},
-        methods: {
-            ...mapMutations('m_user',['updateUserInfo','updateOpenid','updateSwiperList', 'updateToken','updateAddress', 'updatePickPoint', 'updateCode','updateTabBarList']),
-           ...mapMutations('m_order',['setOrdersNonValide','setOrdersNonPayer','setOrdersNonLivrer','setOrdersRembouse', 'updateOrderListByOpenId','updateWarehouseRequestByOpenId','updateCatTree','updateOrderList','updateBoxList']),
-	
-            //用户授权之后，获取用户的基础信息
-            getUserInfo(e) {
-                if (e.detail.errMsg == 'getUserInfo:fail auth deny') return uni.$showMsg('您取消了登录授权!')
-                this.updateUserInfo(JSON.stringify(e.detail.userInfo))
-                console.log('userInfo:',e.detail.userInfo)
-                this.getToken(e.detail.userInfo)
-            },
-            async getToken(info) {
-                const [err, res] = await uni.login().catch(err => err)
-                if (err || res.errMsg !== 'login:ok') return uni.$showMsg('登陆失败')
+		computed: {
+			...mapState('m_user', ['token', 'openid'])
+		},
+		methods: {
+			...mapMutations('m_user', ['updateUserInfo', 'updateOpenid', 'updateSwiperList', 'updateToken',
+				'updateAddress', 'updatePickPoint', 'updateCode', 'updateTabBarList'
+			]),
+			...mapMutations('m_order', ['setOrdersNonValide', 'setOrdersNonPayer', 'setOrdersNonLivrer',
+				'setOrdersRembouse', 'updateOrderListByOpenId', 'updateWarehouseRequestByOpenId', 'updateCatTree',
+				'updateOrderList', 'updateBoxList'
+			]),
 
-                const query = {
-                   'avatarUrl': info.avatarUrl,
-                   'nickName': info.nickName,
-                   'js_code': res.code,
-                   'gender': info.gender
-                }
-                
-            const { data:result } =   await uni.$http.get('/wx/users/login', query );
-              if( result.status !== 200 ) return uni.$showMsg( result.msg )     
-              this.updateToken(result.data.token)
-              this.updateOpenid(result.data.openid)
-			  this.updateUserInfo(JSON.stringify(result.data))
-			  this.initTabBar(result.data)
-			  this.getAllorderList()
-			  this.getAllBoxList()
-              if(result.data.userRoles == 3) {
-				this.updateAddress(result.data.address)
-				this.updateCode(result.data.code)
-				this.updatePickPoint(result.data.ppName)  
-				//this.initSwiperDate()
-			  }
-			  
- 
-            //获取后台返回的token,保存到storage中
-            uni.$showMsg('登录成功!')
-			this.getWarehouseRequest()
-			this.initCatTree()
+			//用户授权之后，获取用户的基础信息
+			getUserInfo(e) {
+				if (e.detail.errMsg == 'getUserInfo:fail auth deny') return uni.$showMsg('您取消了登录授权!')
+				this.updateUserInfo(JSON.stringify(e.detail.userInfo))
+				console.log('userInfo:', e.detail.userInfo)
+				this.getToken(e.detail.userInfo)
+			},
+			async getToken(info) {
+				const [err, res] = await uni.login().catch(err => err)
+				if (err || res.errMsg !== 'login:ok') return uni.$showMsg('登陆失败')
 
-            } ,
-			
+				const query = {
+					'avatarUrl': info.avatarUrl,
+					'nickName': info.nickName,
+					'js_code': res.code,
+					'gender': info.gender
+				}
+
+				const {
+					data: result
+				} = await uni.$http.get('/wx/users/login', query);
+				if (result.status !== 200) return uni.$showMsg(result.msg)
+				this.updateToken(result.data.token)
+				this.updateOpenid(result.data.openid)
+				this.updateUserInfo(JSON.stringify(result.data))
+				this.initTabBar(result.data)
+				this.getAllorderList()
+				this.getAllBoxList()
+				if (result.data.userRoles == 3) {
+					this.updateAddress(result.data.address)
+					this.updateCode(result.data.code)
+					this.updatePickPoint(result.data.ppName)
+					//this.initSwiperDate()
+				}
+
+
+				//获取后台返回的token,保存到storage中
+				uni.$showMsg('登录成功!')
+				this.getWarehouseRequest()
+				this.initCatTree()
+
+			},
+
 			async initCatTree() {
-			    const {data: res} = await uni.$http.get('/wx/orders/catlist')
-			    if (res.status != 200) return uni.$showMsg('查詢商品類別列表失败!')
-			     console.log('res:', res.data)
-			     this.updateCatTree(res.data)
-			} ,
-			
+				const {
+					data: res
+				} = await uni.$http.get('/wx/orders/catlist')
+				if (res.status != 200) return uni.$showMsg('查詢商品類別列表失败!')
+				console.log('res:', res.data)
+				this.updateCatTree(res.data)
+			},
+
 			async getAllorderList() {
-			  const {data: res} = await uni.$http.get('/wx/orders/getAllorderList')
-			  if (res.status != 200) return uni.$showMsg('查詢訂單列表失败!')
-			   console.log('res:', res.data)
-			   this.updateOrderList(res.data)  
+				const {
+					data: res
+				} = await uni.$http.get('/wx/orders/getAllorderList')
+				if (res.status != 200) return uni.$showMsg('查詢訂單列表失败!')
+				console.log('res:', res.data)
+				this.updateOrderList(res.data)
 			},
 			initTabBar(userInfo) {
-				if(userInfo.userRoles == 3) {
-					this.updateTabBarList( getApp().globalData.clientTabBarList )
-				}else {
-					this.updateTabBarList( getApp().globalData.adminTabBarList )
+				if (userInfo.userRoles == 3) {
+					this.updateTabBarList(getApp().globalData.clientTabBarList)
+				} else {
+					this.updateTabBarList(getApp().globalData.adminTabBarList)
 				}
 			},
-				
+			/*
 			async initSwiperDate() {
 			    const {
 			        data: result
@@ -100,86 +114,102 @@
 			    this.updateSwiperList(result.message)
 			    this.initOrders()
 			
+			},*/
+			async initSwiperDate() {
+				let imageList = [{
+						"image_src": "/static/banner-01.jpg"
+					},
+					{
+						"image_src": "/static/banner-02.jpg"
+					},
+					{
+						"image_src": "/static/banner-03.jpg"
+					},
+				]
+
+				this.updateSwiperList(imageList)
+				this.initOrders()
+
 			},
-			
+
 			async getAllBoxList() {
 				const {
-				    data: result
+					data: result
 				} = await uni.$http.get('/wx/box/list')
 				if (result.status !== 200) return uni.$showMsg('查询箱子列表失败')
-				 this.updateBoxList(res.data)
+				this.updateBoxList(res.data)
 			},
-			
+
 			async initOrders() {
-			    const {
-			        data: res
-			    } = await uni.$http.get('/wx/orders/getAllOrderListByOpenId?openId=' + this.openid)
-			    if (res.status !== 200) return uni.$showMsg()
-			    this.orderList = res.data
-			    this.updateOrderListByOpenId(res.data)
-			    this.initOrderList()
-			
+				const {
+					data: res
+				} = await uni.$http.get('/wx/orders/getAllOrderListByOpenId?openId=' + this.openid)
+				if (res.status !== 200) return uni.$showMsg()
+				this.orderList = res.data
+				this.updateOrderListByOpenId(res.data)
+				this.initOrderList()
+
 			},
-			
+
 			initOrderList() {
-			    this.ordersNonValide = this.orderList.filter(x => x.orderStatus === 2)
-			    this.setOrdersNonValide(this.ordersNonValide)
-			    this.ordersNonPayer = this.orderList.filter(x => x.orderStatus === 1)
-			    console.log('ordersNonPayer:', this.ordersNonPayer)
-			    this.setOrdersNonPayer(this.ordersNonPayer)
-			    this.ordersNonLivrer = this.orderList.filter(x => x.payStatus === 20)
-			    this.setOrdersNonLivrer(this.ordersNonLivrer)
-			    this.ordersRembourse = this.orderList.filter(x => x.payStatus === 40)
-			    this.setOrdersRembouse(this.ordersRembourse)
-			
+				this.ordersNonValide = this.orderList.filter(x => x.orderStatus === 2)
+				this.setOrdersNonValide(this.ordersNonValide)
+				this.ordersNonPayer = this.orderList.filter(x => x.orderStatus === 1)
+				console.log('ordersNonPayer:', this.ordersNonPayer)
+				this.setOrdersNonPayer(this.ordersNonPayer)
+				this.ordersNonLivrer = this.orderList.filter(x => x.payStatus === 20)
+				this.setOrdersNonLivrer(this.ordersNonLivrer)
+				this.ordersRembourse = this.orderList.filter(x => x.payStatus === 40)
+				this.setOrdersRembouse(this.ordersRembourse)
+
 			},
-			
+
 			async getWarehouseRequest() {
 				const {
-				    data: res
+					data: res
 				} = await uni.$http.get('/wx/users/getWarehouseRequestByOpenId?openId=' + this.openid)
 				if (res.status !== 200) return uni.$showMsg()
 				this.warehouseRequestList = res.data
 				this.updateWarehouseRequestByOpenId(res.data)
 			}
-        }
-    }
+		}
+	}
 </script>
 
 <style lang="scss">
-    .login-container {
-        height: 550px;
-        background-color: #F8F8F8;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        overflow: hidden;
+	.login-container {
+		height: 550px;
+		background-color: #F8F8F8;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		overflow: hidden;
 
-        &::after {
-            content: '';
-            display: block;
-            width: 100%;
-            height: 40px;
-            background-color: white;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            border-radius: 100%;
-            transform: translateY(50%);
-        }
+		&::after {
+			content: '';
+			display: block;
+			width: 100%;
+			height: 40px;
+			background-color: white;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			border-radius: 100%;
+			transform: translateY(50%);
+		}
 
-        .btn-login {
-            width: 90%;
-            border-radius: 100px;
-            margin: 15px 0;
-            background-color: #C00000;
-        }
+		.btn-login {
+			width: 90%;
+			border-radius: 100px;
+			margin: 15px 0;
+			background-color: #C00000;
+		}
 
-        .tips-text {
-            font-size: 12px;
-            color: gray;
-        }
-    }
+		.tips-text {
+			font-size: 12px;
+			color: gray;
+		}
+	}
 </style>
